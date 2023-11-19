d01b53e68b09d1e42843aef9bc773dc6f8aef052
FrederikHolmboe <59177187+FrederikHolmboe@users.noreply.github.com>
2023-11-19 01:56:17 +0100

    Add project files.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace TestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var number = true;
+
+            Assert.True(number, "it doesent work");
+        }
+    }
+}
