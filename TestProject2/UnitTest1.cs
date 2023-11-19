cd57bc2c6e7a71298d3c1b7a4224894ed831b855
FrederikHolmboe <59177187+FrederikHolmboe@users.noreply.github.com>
2023-11-19 10:41:30 +0100

    added new project test

---
+++ TestProject2/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TestProject2
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var boolIsNumber = false;
+            Assert.True(boolIsNumber);
+
+
+
+        }
+    }
+}
