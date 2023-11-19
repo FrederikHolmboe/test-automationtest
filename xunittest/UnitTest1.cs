7c657785f3a3f6b794fddb2f458afa2c46084167
FrederikHolmboe <59177187+FrederikHolmboe@users.noreply.github.com>
2023-11-19 01:59:17 +0100

    test1

---
+++ xunittest/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace xunittest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var boolIsNumber = true;
+            Assert.True(boolIsNumber);
+        }
+    }
+}
