81f378a2603a7460fe867f451fd1127816fe3d1e
FrederikHolmboe <59177187+FrederikHolmboe@users.noreply.github.com>
2023-11-21 18:35:54 +0100

    changes

--- TestProject2/UnitTest1.cs
+++ TestProject2/UnitTest1.cs
@@ -7,10 +7,15 @@ namespace TestProject2
         {
             var boolIsNumber = true;
             Assert.True(boolIsNumber);
+        }
 
+        [Fact]
+        public void test2()
+        {
+            var testtest = new test_automationtest.Functions.Functions();
+            var result = testtest.test1(5, true);
 
-
-
+            Assert.Equal(25, result);
         }
     }
 }
---
+++ test-automationtest/Functions/Functions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_automationtest.Functions
+{
+    public class Functions
+    {
+        public int test1(int test1, bool multiply)
+        {
+            if (multiply)
+                return test1 * test1;
+            else
+                return test1;
+        }
+    }
+}
